fb5e287e8d9ea8c3a8c4138fa819b0bd7fd74830
usmansalim3 <usmansalim3@gmail.com>
2023-12-25 18:17:43 +0530

    refractor : orders route

--- server.js
+++ server.js
@@ -265,26 +265,19 @@ app.get("/addresses/:userId", async (req, res) => {
 //endpoint to store all the orders
 app.post("/orders", async (req, res) => {
   try {
-    const { userId, cartItems, totalPrice, shippingAddress, paymentMethod } =
+    const { userId, products : p, totalPrice, shippingAddress, paymentMethod } =
       req.body;
-
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
     //create an array of product objects from the cart Items
-    const products = cartItems.map((item) => ({
-      name: item?.title,
-      quantity: item.quantity,
-      price: item.price,
-      image: item?.image,
-    }));
 
     //create a new Order
     const order = new Order({
       user: userId,
-      products: cartItems,
+      products: p,
       totalPrice: totalPrice,
       shippingAddress: shippingAddress,
       paymentMethod: paymentMethod,
@@ -293,7 +286,7 @@ app.post("/orders", async (req, res) => {
     await user.save();
     await order.save();
 
-    res.status(200).json({ message: "Order created successfully!" });
+    res.status(200).json({ message: "Order created successfully!"});
   } catch (error) {
     console.log("error creating orders", error);
     res.status(500).json({ message: "Error creating orders" });
